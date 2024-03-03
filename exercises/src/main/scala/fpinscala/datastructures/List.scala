64ac3a2fa20b319c67f1cd62656c36d9a06e259a
mosaicthej <mij623@usask.ca>
2024-03-02 22:41:17 -0600

    added datastructure solutions

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -50,19 +50,120 @@ object List { // `List` companion object. Contains functions for creating and wo
     foldRight(ns, 1.0)(_ * _) // `_ * _` is more concise notation for `(x,y) => x * y`; see sidebar
 
 
-  def tail[A](l: List[A]): List[A] = ???
+  def tail[A](l: List[A]): List[A] = 
+    l match {
+      case Cons(_,t)=>t
+    }
+    
+  def setHead[A](l: List[A], h: A): List[A] = 
+    l match {
+      case Cons(_,t)=> Cons(h, t)
+    }
 
-  def setHead[A](l: List[A], h: A): List[A] = ???
+  def drop[A](l: List[A], n: Int): List[A] = 
+    if (n<=0) l
+    else l match{
+      case Nil => Nil
+      case Cons(_,t) => drop(t, n-1)
+    }
 
-  def drop[A](l: List[A], n: Int): List[A] = ???
+  def dropWhile0[A](l: List[A], f: A => Boolean): List[A] =
+    l match {
+      case Cons(h,t) => if (f(h)) dropWhile(t, f) else l
+    }
 
-  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = ???
+  def dropWhile[A](l: List[A], f: A => Boolean): List[A] =
+    l match {
+      case Cons(h,t) if f(h) => dropWhile(t, f)
+      case _ => l
+    }
 
-  def init[A](l: List[A]): List[A] = ???
+  def init[A](l: List[A]): List[A] = 
+    l match {
+      case Cons(_, Nil) => Nil
+      case Cons(h, t) => Cons(h, init(t))
+    }
 
-  def length[A](l: List[A]): Int = ???
+  def init2[A](l: List[A]): List[A] = {
+    import collection.mutable.ListBuffer
+    val buf = new ListBuffer[A]
+    @annotation.tailrec
+    def init2Help(cur: List[A]): List[A] = 
+      cur match {
+        case Cons(_,Nil) => List(buf.toList: buf.toList: _*)
+        case Cons(h,t) => buf += h ; go(t)
+      }
+    init2Help()
+  }
 
-  def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B = ???
+  def length[A](l: List[A]): Int = 
+    foldRight(l, 0) ( (_,accu)=>accu+1 ) 
+  /* a function that consumes HEAD, returns accu+1 
+   * where accu is 0 upon nil list */
+  
+  @annotation.tailrec
+  def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B =
+    l match{
+      case Nil => z // zero val
+      case Cons(h,t) => foldLeft(t, f(z,h))(f)
+      // l-> (cdr l), z->f(z,h)
+      // apply f on zero-val and h first (1 stack deep)
+      // a function that 
+
+      /* [1 2 3 4]
+       * tmp_1 = f(z, 1)
+       * tmp_2 = f(tmp_1, 2)
+       * tmp_3 = f(tmp_2, 3)
+       * tmp_4 = f(tmp_3, 4)
+       * tmp_5 = f(tmp_4, Nil) = tmp_4
+       * res = tmp5 = tmp4
+       * */
+    }
+  def sum3(l:List[Int]) = foldLeft(l,0)(_ + _) 
+  // first add head with 0, then add next with head,
+  // then add ... onto the accumulator
+  //
+  def product3(l:List[Int]) = foldLeft(l, 1.0)(_ * _)
+
+  def length2[A](l:List[A]): Int=foldLeft(l,0)((soFar,_)=>soFar+1)
+  /*length2([1 2 3 4])
+   *
+   * FL([1 2 3 4], 0)  
+   * FL([2 3 4], 1)    
+   * FL([3 4], 2)     
+   * FL([4], 3)        
+   * FL(Nil, 4)
+   * 4   *
+   * */
+  def reverse[A](l:List[A]): List[A] =
+    foldLeft(l, List[A]()) ((intermediate, h)=>Cons(h,intermediate))
+
+  /*
+   * foldRight on [1 2 3 4] would be do (after linearize the code)
+   * -> f(1,f(2,f(3,f(4,z))))  | -> on foldLeft of the same 
+   * which is,                 | [1 2 3 4] same list will be
+   * 1. tmp_1 = f(4,z)         | 1. tmp_1 = f(z, 1)
+   * 2. tmp_2 = f(3, tmp_1)    | 2. tmp_2 = f(tmp_1, 2)
+   * 3. tmp_3 = f(2, tmp_2)    | 3. tmp_3 = f(tmp_2, 3)
+   * 4. tmp_4 = f(1, tmp_3)    | 4. tmp_4 = f(tmp_3, 4)
+   * 5. result=tmp_4           | 5. result= tmp_4 = f(tmp_4, Nil)
+   *
+   * observe with above, it is just work on fL on reverse order.
+   * Also, 2 args of f would switch order
+   * */
+  def foldRightByFoldLeft[A,B](l:List[A], z:B)(f:(A,B)=>B): B=
+    foldLeft(reverse(l), z)((arg1, arg2)=>f(arg2, arg1))
+
+
+  def foldRightByFoldLeft_1[A,B](l: List[A], z:B)(f:(A,B)=>B): B=
+    foldLeft(l, (b:B)=>b ) /* zero val is identity func.
+     now fL still waiting for `f: BxA => B` 
+       (f curr (car L)) -> newCurr
+    */ ((g, a) => /* a func takes 2 args and ret another func that
+      
+    */   b => g(f(a,b))) 
+      (z)
+  
 
   def map[A,B](l: List[A])(f: A => B): List[B] = ???
 }
